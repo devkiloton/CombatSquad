00b43f985f3ead13d91ab02c861baab5b2e5b7ab
devkiloton <dev.kiloton@gmail.com>
2021-07-09 10:51:00 -0700

    Jumps

--- CombatSquad/Assets/Assets/Scripts/PlayerController.cs
+++ CombatSquad/Assets/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,98 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform viewPoint;
     [SerializeField]
     private float mouseSensivity;
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float runSpeed = 8f;
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField]
+    private float gravityMod = 2.5f;
+    private float activeMoveSpeed;
+    private Transform viewPoint;
+    private CharacterController chaCon;
+    private Camera cam;
+    private Transform groundCheckPoint;
+    private bool isGrounded;
+    private LayerMask groundLayers;
     private float verticalRotStore;
     private Vector2 mouseInput;
-    void Start()
+    private Vector3 moveDir;
+    private Vector3 movement;
+
+    private void Start()
     {
-       viewPoint = GameObject.FindGameObjectWithTag("ViewPoint").transform;
+        groundCheckPoint = gameObject.GetComponentInChildren<Transform>().Find("GroundCheckPoint");
+        viewPoint = gameObject.GetComponentInChildren<Transform>().Find("ViewPoint");
+        groundLayers = LayerMask.GetMask("Ground");
+        chaCon = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        cam = Camera.main;
     }
 
-    void Update()
+    private void Update()
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensivity;
+
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
         verticalRotStore -= mouseInput.y;
         verticalRotStore = Mathf.Clamp(verticalRotStore, -60f, 60f);
+
         viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            activeMoveSpeed = runSpeed;
+        }
+        else
+        {
+            activeMoveSpeed = moveSpeed;
+        }
+
+        float yVel = movement.y;
+
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+
+        movement.y = yVel;
+
+        if (chaCon.isGrounded)
+        {
+            movement.y = 0f;
+        }
+
+        isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .25f, groundLayers);
+        //print(isGrounded);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            movement.y = jumpForce;
+        }
+
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+
+        chaCon.Move(movement * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if(Cursor.lockState == CursorLockMode.None)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
     }
 }
